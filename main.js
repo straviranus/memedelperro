8be88af839f1c7cf2a5c98e4defe85ecf622a380
juan casco <tomeoamigoivan@gmail.com>
2020-06-30 19:32:41 -0300

    primera version

---
+++ main.js
@@ -0,0 +1,12 @@
+  function meme () {
+var grande= document.getElementById("perrogrande").value;
+var chico= document.getElementById("perrochico").value;
+console.log(grande);
+var text1=grande;
+var text2= chico;
+
+document.getElementById("big").innerHTML = text1;
+document.getElementById("small").innerHTML = text2;
+
+  }
+
